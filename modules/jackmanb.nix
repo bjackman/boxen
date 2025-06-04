c689f84ca518ac10c4f2294a68d07e5db721c54f
Brendan Jackman <jackmanb@google.com>
2025-06-04 12:58:33 +0000

    jackmanb: Add ~/.local/bin/ to PATH

--- modules/jackmanb.nix
+++ modules/jackmanb.nix
@@ -3,9 +3,14 @@
     username = "jackmanb";
     homeDirectory = "/usr/local/google/home/jackmanb";
 
-    # I don't know why this is necessary but for some reason I don't get PATH set
-    # up on gLinux.
-    sessionPath = [ "${config.home.profileDirectory}/bin" ];
+    sessionPath = [
+      # I don't know why this is necessary but for some reason I don't get PATH set
+      # up on gLinux.
+      "${config.home.profileDirectory}/bin"
+      # "Temporary hack": make stuff I've installed with "pipx" etc visible,
+      # until I can migrate to a proper package manager.
+      "${config.home.homeDirectory}/.local/bin"
+    ];
   };
 
   common.fishConfigDirs = [ ../files/jackmanb/config/fish ];
