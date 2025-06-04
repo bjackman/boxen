e290554f0ec529945d2ba3342b2b18b637706649
Brendan Jackman <jackmanb@google.com>
2025-06-04 11:39:58 +0000

    jackmanb: Fix formatting

--- modules/jackmanb.nix
+++ modules/jackmanb.nix
@@ -2,9 +2,10 @@
 {
   home = {
     username = "jackmanb";
+    homeDirectory = "/usr/local/google/home/jackmanb";
 
     # I don't know why this is necessary but for some reason I don't get PATH set
     # up on gLinux.
-    sessionPath = [ "${config.home.profileDirectory}/bin" ];   homeDirectory = "/usr/local/google/home/jackmanb";
+    sessionPath = [ "${config.home.profileDirectory}/bin" ];
   };
 }
