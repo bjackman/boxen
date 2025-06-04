ac2e3e358988f5da673613099a31249de3fc7bf9
Brendan Jackman <jackmanb@google.com>
2025-06-04 12:23:14 +0000

    common: Add logic to combine Fish directories from multiple modules

--- modules/common.nix
+++ modules/common.nix
@@ -1,36 +1,54 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, lib, ... }:
 {
-  home = {
-    stateVersion = "25.05";
-
-    packages = with pkgs; [ hello ];
-
-    file = {
-      # You can configure some Fish stuff through Nix, but experimentally it
-      # seems you can also just dump files into the home directory and things
-      # work OK.
-      ".config/fish/" = {
+  options = {
+    common.fishConfigDirs = lib.mkOption {
+      type = lib.types.listOf lib.types.pathInStore;
+      default = [
         # Note awkward relative path here. Alternative would be to communicate a
         # base path for these files via specialArgs based on the flake's `self`.
-        source = ../files/common/config/fish;
-        recursive = true;
-      };
+        ../files/common/config/fish
+      ];
+      description = "Derivations producing directories with fish configs, will be combined into a single config using symlinkJoin.";
+    };
+  };
+
+  config = {
+    home = {
+      stateVersion = "25.05";
+
+      packages = with pkgs; [ hello ];
+
+      file =
+        let
+          fishConfig = pkgs.symlinkJoin {
+            name = "fish-config";
+            paths = [ config.common.fishConfigDirs ];
+          };
+        in
+          {
+            # You can configure some Fish stuff through Nix, but experimentally it
+            # seems you can also just dump files into the home directory and things
+            # work OK.
+            ".config/fish/" = {
+              source = fishConfig;
+              recursive = true;
+            };
+          };
+
+      sessionVariables = { EDITOR = "vim"; };
     };
 
-    sessionVariables = { EDITOR = "vim"; };
-  };
+    programs.home-manager.enable = true;
 
-  programs.home-manager.enable = true;
-
-  programs.fish = {
-    enable = true;
-    plugins = [
-      {
-        name = "z";
-        src = pkgs.fishPlugins.z.src;
-      }
-    ];
+    programs.fish = {
+      enable = true;
+      plugins = [
+        {
+          name = "z";
+          src = pkgs.fishPlugins.z.src;
+        }
+      ];
+    };
+    programs.bash.enable = true;
   };
-  programs.bash.enable = true;
 }
