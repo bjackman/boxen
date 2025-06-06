9ef7ac677e24eadf8b558eb77a69860f0bf71d5a
Brendan Jackman <bhenryj0117@gmail.com>
2025-06-06 11:37:48 +0000

    lkml/aerc: Add UI configuration

--- modules/lkml.nix
+++ modules/lkml.nix
@@ -20,59 +20,103 @@
       default = "${config.home.homeDirectory}/lkml";
     };
   };
-  config = {
-    programs.notmuch = {
-      enable = true;
-      # No option to directly override the default which is
-      # config.accounts.email.maildirBasePath.
-      extraConfig = {
-        database.path = config.lkml.maildirBasePath;
-      };
-    };
-
-    programs.aerc = {
-      enable = true;
-      # aerc is fussy about config permissions since you might put creds in
-      # there. Nix doesn't support having the cautious permissions, but we won't
-      # put creds in it (they would be leaked into the Nix store anyway).
-      extraConfig.general.unsafe-accounts-conf = true;
-    };
-
-    # TODO: Defining packages directly here is messy. But I haven't figured out
-    # the proper way to organise this.
-    home.packages =
-      let
-        # Copied from
-        # https://stackoverflow.com/questions/43837691/how-to-package-a-single-python-script-with-nix
-        # Seems reasonably sensible??
-        notmuch-propagate-mute = pkgs.stdenv.mkDerivation {
-          name = "notmuch-propagate-mute";
-          propagatedBuildInputs = [
-            (pkgs.python3.withPackages (pythonPackages: [ pythonPackages.notmuch ]))
-          ];
-          dontUnpack = true;
-          installPhase = "install -Dm755 ${../src/notmuch_propagate_mute.py} $out/bin/notmuch-propagate-mute";
+  config =
+    # TODO: can't be bothered to figure out multiple addresses, assert
+    # there is only one.
+    let
+      account =
+        let accounts = lib.attrValues config.accounts.email.accounts;
+        in (assert (builtins.length accounts == 1); (lib.head accounts));
+    in {
+      programs.notmuch = {
+        enable = true;
+        # No option to directly override the default which is
+        # config.accounts.email.maildirBasePath.
+        extraConfig = {
+          database.path = config.lkml.maildirBasePath;
         };
-      in
-      [
-        # Expose the package directly for testing.
-        notmuch-propagate-mute
-        (pkgs.writeShellApplication {
-          name = "get-lkml";
-          # For lei
-          runtimeInputs = [
-            pkgs.public-inbox
-            pkgs.notmuch
-            notmuch-propagate-mute
-          ];
-          text =
-            # TODO: can't be bothered to figure out multiple addresses, assert
-            # there is only one.
-            let
-              account =
-                let accounts = lib.attrValues config.accounts.email.accounts;
-                in (assert (builtins.length accounts == 1); (lib.head accounts));
-            in ''
+      };
+
+      programs.aerc = {
+        enable = true;
+        extraConfig = {
+          # aerc is fussy about config permissions since you might put creds in
+          # there. Nix doesn't support having the cautious permissions, but we won't
+          # put creds in it (they would be leaked into the Nix store anyway).
+          general.unsafe-accounts-conf = true;
+
+          # Describes the format for each row in a mailbox view. This is a comma
+          # separated list of column names with an optional align and width suffix. After
+          # the column name, one of the '<' (left), ':' (center) or '>' (right) alignment
+          # characters can be added (by default, left) followed by an optional width
+          # specifier. The width is either an integer representing a fixed number of
+          # characters, or a percentage between 1% and 99% representing a fraction of the
+          # terminal width. It can also be one of the '*' (auto) or '=' (fit) special
+          # width specifiers. Auto width columns will be equally attributed the remaining
+          # terminal width. Fit width columns take the width of their contents. If no
+          # width specifier is set, '*' is used by default.
+          #
+          # Default: flags:4,name<20%,subject,date>=
+          ui = {
+            index-columns = "addressed:1,flags:4,name<20%,subject<60%,date>,tags";
+
+            # Show Notmuch tags in a column, excluding the ones that are already implied by
+            # the context of the UI.
+            column-tags = ''{{map .Labels (exclude "inbox" ) (exclude "read") (exclude "replied") | join " "}}'';
+            # Show a single character 'A' if I'm personally addressed, otherwise or ' ' .
+            # Works by joining together all the To and Cc emails into a single string and
+            # then searching. Maybe dumb, whatever.
+            column-addressed = lib.trim ''
+              {{ if contains "${account.address}" (printf "%s%s" (.To | emails | join "") (.Cc | emails | join "")) }}A{{ else }} {{ end }}
+            '';
+
+            # Sort the thread siblings according to the sort criteria for the messages. If
+            # sort-thread-siblings is false, the thread siblings will be sorted based on
+            # the message UID in ascending order. This option is only applicable for
+            # client-side threading with a backend that enables sorting. Note that there's
+            # a performance impact when sorting is activated.
+            #
+            # Default: false
+            sort-thread-siblings = true;
+
+            #
+            # Enable a threaded view of messages. If this is not supported by the backend
+            # (IMAP server or notmuch), threads will be built by the client.
+            #
+            # Default: false
+            threading-enabled = true;
+          };
+        };
+      };
+
+      # TODO: Defining packages directly here is messy. But I haven't figured out
+      # the proper way to organise this.
+      home.packages =
+        let
+          # Copied from
+          # https://stackoverflow.com/questions/43837691/how-to-package-a-single-python-script-with-nix
+          # Seems reasonably sensible??
+          notmuch-propagate-mute = pkgs.stdenv.mkDerivation {
+            name = "notmuch-propagate-mute";
+            propagatedBuildInputs = [
+              (pkgs.python3.withPackages (pythonPackages: [ pythonPackages.notmuch ]))
+            ];
+            dontUnpack = true;
+            installPhase = "install -Dm755 ${../src/notmuch_propagate_mute.py} $out/bin/notmuch-propagate-mute";
+          };
+        in
+        [
+          # Expose the package directly for testing.
+          notmuch-propagate-mute
+          (pkgs.writeShellApplication {
+            name = "get-lkml";
+            # For lei
+            runtimeInputs = [
+              pkgs.public-inbox
+              pkgs.notmuch
+              notmuch-propagate-mute
+            ];
+            text = ''
               lei q -I https://lore.kernel.org/all/ -o ${config.lkml.maildirBasePath} \
                 --threads --dedupe=mid --augment \
                 '(a:${account.address} OR a:linux-mm@kvack.org OR a:x86@kernel.org) AND d:2025-04'
@@ -80,7 +124,7 @@
               notmuch-propagate-mute \
                 --email ${account.address} --db-path ${config.lkml.maildirBasePath}
             '';
-        })
-      ];
-  };
-}
+          })
+        ];
+    };
+  }
