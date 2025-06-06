446bfd651d26de1663298a0d8e785e207e92152e
Brendan Jackman <bhenryj0117@gmail.com>
2025-06-06 09:42:47 +0000

    lkml: Package notmuch-propagate-mute

--- modules/lkml.nix
+++ modules/lkml.nix
@@ -26,21 +26,39 @@
       };
     };
 
-    home.packages = [
-      (pkgs.writeShellApplication {
-        name = "get-lkml";
-        # For lei
-        runtimeInputs = [
-          pkgs.public-inbox
-          pkgs.notmuch
-        ];
-        text = ''
-          lei q -I https://lore.kernel.org/all/ -o ${config.lkml.maildirBasePath} \
-            --threads --dedupe=mid --augment \
-            '(a:jackmanb@google.com OR a:linux-mm@kvack.org OR a:x86@kernel.org) AND d:2025-04'
-          notmuch new
-        '';
-      })
-    ];
+    # TODO: Defining packages directly here is messy. But I haven't figured out
+    # the proper way to organise this.
+    home.packages =
+      let
+        # Copied from
+        # https://stackoverflow.com/questions/43837691/how-to-package-a-single-python-script-with-nix
+        # Seems reasonably sensible??
+        notmuch-propagate-mute = pkgs.stdenv.mkDerivation {
+          name = "notmuch-propagate-mute";
+          propagatedBuildInputs = [
+            (pkgs.python3.withPackages (pythonPackages: [ pythonPackages.notmuch ]))
+          ];
+          dontUnpack = true;
+          installPhase = "install -Dm755 ${../src/notmuch_propagate_mute.py} $out/bin/notmuch-propagate-mute";
+        };
+      in
+      [
+        # Expose the package directly for testing.
+        notmuch-propagate-mute
+        (pkgs.writeShellApplication {
+          name = "get-lkml";
+          # For lei
+          runtimeInputs = [
+            pkgs.public-inbox
+            pkgs.notmuch
+          ];
+          text = ''
+            lei q -I https://lore.kernel.org/all/ -o ${config.lkml.maildirBasePath} \
+              --threads --dedupe=mid --augment \
+              '(a:jackmanb@google.com OR a:linux-mm@kvack.org OR a:x86@kernel.org) AND d:2025-04'
+            notmuch new
+          '';
+        })
+      ];
   };
 }
