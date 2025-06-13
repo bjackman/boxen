1f6f35c4ceaf6624a002a5912dbe9b47cb98bfed
Brendan Jackman <jackmanb@google.com>
2025-06-13 13:24:15 +0000

    fish: Add -i arg for addr2line
    This arg means "be useful instead of useless" i.e. consider inlined
    functions.

--- files/common/config/fish/conf.d/kernel.fish
+++ files/common/config/fish/conf.d/kernel.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    abbr --add a2l "addr2line -e vmlinux"
+    abbr --add a2l "addr2line -ie vmlinux"
     abbr --add grib "git rebase --interactive (b4 prep --show-info start-commit)"
 end
 
