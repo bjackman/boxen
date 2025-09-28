29ab4e9e4e452cbd76345b5adc0eab2f8d167869
Brendan Jackman <bhenryj0117@gmail.com>
2025-09-28 17:48:33 +0200

    hm/brendan: Install spotify

--- hm_modules/brendan.nix
+++ hm_modules/brendan.nix
@@ -15,6 +15,7 @@
       pstree
       btop
       libnotify  # Useful for testing when faffing around with hyprnotify
+      spotify
     ];
   };
   programs.git.userEmail = "bhenryj0117@gmail.com";
