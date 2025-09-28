676a6ad2c0cca36c23f683a18a59556d53603491
Brendan Jackman <bhenryj0117@gmail.com>
2025-09-28 21:54:18 +0200

    hm/hyprland: Simplify window bordering (no rounding, less gaps)
    Also disable shadow and blur which I don't think were doing anything for
    whatever reason

--- hm_modules/hyprland.nix
+++ hm_modules/hyprland.nix
@@ -344,7 +344,7 @@ in
       ];
       general = {
         gaps_in = 5;
-        gaps_out = 20;
+        gaps_out = 5;
         border_size = 2;
         # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
         "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
@@ -356,25 +356,9 @@ in
         layout = "dwindle";
       };
       decoration = {
-        rounding = 10;
-        rounding_power = 2;
         # Change transparency of focused and unfocused windows
         active_opacity = 1.0;
         inactive_opacity = 1.0;
-        shadow = {
-          enabled = true;
-          range = 4;
-          render_power = 3;
-          color = "rgba(1a1a1aee)";
-        };
-        # https://wiki.hyprland.org/Configuring/Variables/#blur
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-
-          vibrancy = 0.1696;
-        };
       };
       # https://wiki.hyprland.org/Configuring/Variables/#animations
       animations = {
