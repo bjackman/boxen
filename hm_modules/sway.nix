65d0143364b8489f19c83edc06b07e3f4d3757ad
Brendan Jackman <bhenryj0117@gmail.com>
2025-09-30 16:31:12 +0200

    sway: Refactor workspace bindings

--- hm_modules/sway.nix
+++ hm_modules/sway.nix
@@ -82,28 +82,6 @@ in
         "${modifier}+Shift+space" = "floating toggle";
         "${modifier}+space" = "focus mode_toggle";
 
-        "${modifier}+1" = "workspace number 1";
-        "${modifier}+2" = "workspace number 2";
-        "${modifier}+3" = "workspace number 3";
-        "${modifier}+4" = "workspace number 4";
-        "${modifier}+5" = "workspace number 5";
-        "${modifier}+6" = "workspace number 6";
-        "${modifier}+7" = "workspace number 7";
-        "${modifier}+8" = "workspace number 8";
-        "${modifier}+9" = "workspace number 9";
-        "${modifier}+0" = "workspace number 10";
-
-        "${modifier}+Shift+1" = "move container to workspace number 1";
-        "${modifier}+Shift+2" = "move container to workspace number 2";
-        "${modifier}+Shift+3" = "move container to workspace number 3";
-        "${modifier}+Shift+4" = "move container to workspace number 4";
-        "${modifier}+Shift+5" = "move container to workspace number 5";
-        "${modifier}+Shift+6" = "move container to workspace number 6";
-        "${modifier}+Shift+7" = "move container to workspace number 7";
-        "${modifier}+Shift+8" = "move container to workspace number 8";
-        "${modifier}+Shift+9" = "move container to workspace number 9";
-        "${modifier}+Shift+0" = "move container to workspace number 10";
-
         "${modifier}+Shift+minus" = "move scratchpad";
         "${modifier}+minus" = "scratchpad show";
 
@@ -112,7 +90,28 @@ in
           "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
 
         "${modifier}+r" = "mode resize";
-      };
+      }
+      // (
+        let
+          workspaces = {
+            "1" = "number 1";
+            "2" = "number 2";
+            "3" = "number 3";
+            "4" = "number 4";
+            "5" = "number 5";
+            "6" = "number 6";
+            "7" = "number 7";
+            "8" = "number 8";
+            "9" = "number 9";
+          };
+        in
+        (lib.mapAttrs' (
+          key: workspace: lib.nameValuePair "${modifier}+${key}" "workspace ${workspace}"
+        ) workspaces)
+        // (lib.mapAttrs' (
+          key: workspace: lib.nameValuePair "${modifier}+shift+${key}" "move window to workspace ${workspace}"
+        ) workspaces)
+      );
     };
     # Include distro-local stuff. On NixOS this includes something important.
     extraConfig = "include /etc/sway/config.d/*";
