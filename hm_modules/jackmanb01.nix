4b2df4870de19fb3b2f1755c55dea5edca10fb9d
Brendan Jackman <jackmanb@google.com>
2025-09-30 11:50:23 +0200

    jackmanb01: Set up hard-coded monitor layout for Sway

--- hm_modules/jackmanb01.nix
+++ hm_modules/jackmanb01.nix
@@ -9,4 +9,13 @@
     ./jackmanb.nix
     ./sway.nix
   ];
+
+  wayland.windowManager.sway.config.output = {
+    # Center monitor
+    "Lenovo Group Limited P27h-20 V906YLCP" = { position = "0 0"; };
+    # To the right of the Lenovo one
+    "Samsung Electric Company LS27A600U HNMR402251" = { position = "2560 0"; };
+    # Below the lenovo one
+    "BOE 0x0C00 Unknown" = { position = "0 1440"; };
+  };
 }
