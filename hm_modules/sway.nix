6d2a9f7f6dde1e38ff09ecbfa3343a447f84e0ae
Brendan Jackman <jackmanb@google.com>
2025-10-03 19:04:47 +0200

    sway: Drop stale command

--- hm_modules/sway.nix
+++ hm_modules/sway.nix
@@ -99,8 +99,6 @@ in
           background = "${../hm_files/common/clouds.png} fill";
         };
         input."type:touchpad".natural_scroll = "enable";
-        # Copy default keybindings from
-        # https://github.com/NixOS/nixpkgs/blob/d916df777523d75f7c5acca79946652f032f633e/nixos/modules/programs/wayland/sway.nix
         keybindings = {
           "${modifier}+Return" = "exec ${config.wayland.windowManager.sway.config.terminal}";
           "${modifier}+t" = "kill";
