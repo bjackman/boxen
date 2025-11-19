473037fb9510a7c9a89331357eda7d70cd7f8d8d
Brendan Jackman <jackmanb@google.com>
2025-11-19 10:48:32 +0100

    vscode: Don't install it

--- hm_modules/vscode.nix
+++ hm_modules/vscode.nix
@@ -3,7 +3,9 @@
   ...
 }:
 {
-  programs.vscode.enable = true;
+  # Just like in zed.nix, we don't set programs.vscode.enable unless on NixOS,
+  # use the distro's version instead.
+
   xdg.configFile = {
     "Code/User/settings.json" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.bjackman.configCheckout}/hm_files/common/config/Code/User/settings.json";
