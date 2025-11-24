42d49a8b217ddfb7a8286ad942c3af1edb58d366
brendan <bhenryj0117@gmail.com>
2025-11-24 16:12:05 +0000

    nixos/fw13: Trying to build minimal installer

--- nixos_modules/fw13/default.nix
+++ nixos_modules/fw13/default.nix
@@ -1,17 +1,25 @@
-{ config, pkgs, modulesPath, ... }:
+{
+  config,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
   imports = [
     # Experimental: Trying to build an installer by just initially setting up me
     # configuration to be hardcoded to build one...
-    (modulesPath + "/cd-dvd/installation-cd-minimal.nix")
+    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
     ../brendan.nix
     ../pc.nix
     ../sway.nix
-    ../impermanence.nix
+    # Can't use this module until we've set up boot.fileSystems...
+    # ../impermanence.nix
   ];
 
   networking.hostName = "fw13";
+  # Trying to get installer image to build, doesn't seem to like networkmanager?
+  networking.networkmanager.enable = false;
 
   time.timeZone = "Europe/London";
 
