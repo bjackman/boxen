07fdd4ab14be4b9cf39e8e8c4c918669fbfc5edb
Brendan Jackman <jackmanb@google.com>
2025-11-28 10:35:46 +0000

    hm/home-monitors: Add initial Kanshi config

--- hm_modules/home-monitors.nix
+++ hm_modules/home-monitors.nix
@@ -26,4 +26,37 @@
       position = "2560 0";
     };
   };
+
+  services.kanshi = {
+    enable = true;
+
+    settings = [
+      {
+        profile = {
+          name = "laptop-unplugged";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              position = "0,0";
+            }
+          ];
+        };
+      }
+      {
+        profile.name = "sandy-office";
+        profile.outputs = [
+          {
+            criteria = "LG Electronics LG ULTRAFINE 508NTGYKX551";
+            position = "0,0";
+            scale = 1.5;
+          }
+          {
+            criteria = "eDP-1";
+            # To the right of the big boi. X is 3840 / 1.5.
+            position = "2560,0";
+          }
+        ];
+      }
+    ];
+  };
 }
