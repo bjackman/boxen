b97346575337f6ed24372581f1be3cea22d21416
Brendan Jackman <bhenryj0117@gmail.com>
2025-11-30 15:10:21 +0000

    hm/monitors: Drop unused hyprland config

--- hm_modules/monitors.nix
+++ hm_modules/monitors.nix
@@ -4,12 +4,6 @@
   ...
 }:
 {
-  # TODO: Switch to monitorv2 once we have 0.50.
-  wayland.windowManager.hyprland.settings.monitor = [
-    "desc:Dell Inc. DELL P2720DC 81WTK01403MS,preferred,auto,auto"
-    "desc:Google Inc. P2718EC C9240002,preferred,auto-left,auto"
-  ];
-
   # Get the description from "swaymsg -t get_outputs"
   wayland.windowManager.sway.config.output = {
     # On the left
