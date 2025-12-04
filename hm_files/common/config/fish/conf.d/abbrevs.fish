28c860757eb08091d4111e8ea79b7f71b177eb3c
Brendan Jackman <jackmanb@google.com>
2025-12-04 18:14:25 +0900

    hm/fish: Add ju abbrev

--- hm_files/common/config/fish/conf.d/abbrevs.fish
+++ hm_files/common/config/fish/conf.d/abbrevs.fish
@@ -2,3 +2,4 @@ abbr --add hms "home-manager switch"
 abbr --add nrs 'nixos-rebuild switch --flake "$HOME_MANAGER_CONFIG_CHECKOUT#$(hostname)" --use-remote-sudo'
 abbr --add fnd "find . -name"
 abbr --add scu "systemctl --user"
+abbr --add ju "journalctl --user"
