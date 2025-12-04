4998a3b539958765a46af6b69df2f3306d5f564b
Brendan Jackman <jackmanb@google.com>
2025-12-04 18:11:14 +0900

    hm/fish: Add scu abbrev

--- hm_files/common/config/fish/conf.d/abbrevs.fish
+++ hm_files/common/config/fish/conf.d/abbrevs.fish
@@ -1,3 +1,4 @@
 abbr --add hms "home-manager switch"
 abbr --add nrs 'nixos-rebuild switch --flake "$HOME_MANAGER_CONFIG_CHECKOUT#$(hostname)" --use-remote-sudo'
 abbr --add fnd "find . -name"
+abbr --add scu "systemctl --user"
