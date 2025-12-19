0b41bb70e066dda05a848a514bd771535d4e7df0
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-19 18:12:09 +0100

    hm/wezterm: Deduplicate from jackmanb config

--- hm_modules/wezterm.nix
+++ hm_modules/wezterm.nix
@@ -1,33 +1,57 @@
-{ pkgs, config, ... }:
 {
-  programs.wezterm = {
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  options.bjackman.wezterm.extraConfig = lib.mkOption {
+    type = lib.types.str;
+    default = "";
+    description = ''
+      Some Lua code that will be injected at the end of the base config.
+      A Wezterm config_builder object will be in the "config" variable".
+    '';
+  };
+
+  config.programs.wezterm = {
     enable = true;
     extraConfig = ''
       local wezterm = require 'wezterm'
-      return {
-        font_size = 11,
 
-        hide_tab_bar_if_only_one_tab = true,
+      -- This seems dumb but actually it gives you better error messages and
+      -- stuff. This weird imperative style is also kinda paradoxically easier
+      -- to modularise.
+      config = wezterm.config_builder()
 
-        -- https://github.com/wezterm/wezterm/issues/695#issuecomment-820160764
-        adjust_window_size_when_changing_font_size = false,
+      config.font_size = 11
 
-        scrollback_lines = 10000,
+      config.hide_tab_bar_if_only_one_tab = true
+
+      -- https://github.com/wezterm/wezterm/issues/695#issuecomment-820160764
+      configadjust_window_size_when_changing_font_size = false
+
+      config.scrollback_lines = 10000
 
         -- Disable wezterm's shortcuts since I use these keys to control Aerc.
-        keys = {
-          {
-              key = 'PageUp',
-              mods = 'CTRL',
-              action = wezterm.action.DisableDefaultAssignment
-          },
-          {
-              key = 'PageDown',
-              mods = 'CTRL',
-              action = wezterm.action.DisableDefaultAssignment
-          },
+      config.keys = {
+        {
+            key = 'PageUp',
+            mods = 'CTRL',
+            action = wezterm.action.DisableDefaultAssignment
+        },
+        {
+            key = 'PageDown',
+            mods = 'CTRL',
+            action = wezterm.action.DisableDefaultAssignment
         },
       }
+
+      -- begin bjackman.wezterm.extraConfig
+      ${config.bjackman.wezterm.extraConfig}
+      -- end bjackman.wezterm.extraConfig
+
+      return config
     '';
   };
 }
