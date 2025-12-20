f5e4b479631836344e25722a69f9c90a1962e6ea
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-20 17:36:03 +0100

    secrets: Add Pizza host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,10 +5,12 @@ let
   chungito = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaakNfELyvjLLCRwH2U/yQ35HkEW+hEShAD7sn0mCmH brendan@chungito";
   chungito-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTTgMTKXqjE6Vdd5mYMqtU3CxHdTFLVW4TNg3K5dfpo root@chungito";
   brendan-thinkpad = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMi2QRJG+/nM2ekysSUT6h1uNlSmo31ubSK28DrGVezoh2MaPXz6XWMpJtDvr9FHHOVpsCTFxFQ9A7DTqgFy0NxwTHJhK5bevxaWYRkv43H8EMR9pJXYMDAtj7Gk+NNK5ssGZm2P+cTl9r5QZOm0PaVUUeoA/KxbVCNEenOCHM5Lv2RrXGufJL1ukRL6I83fl3ilfgEOz2RBG3QQGahVqYfZq/mfo07U0vad9RX7y6I+8Ap8XSCe33yfO0338yPf0A69p90xtpiJyYyAtVN+0KT552wpMtPjprXt5mrpYDLZvW6vBu0mFGkmDoz3ekb+MmWJVlE9f1VyjHpmA1bRn18gQ73egrGlVWvPHpAJ3gl5bKtc30Md/M4u3tyauDoAnqOs/FAqvClDz1Yav+5Ck5umnDSXXWH/WToX9AUsevjLQq1uB2QJU6oYeEIpEHWC4dUtgPXrX/SYDSGmqA5xOqboyn39oIcNWXTOrqnes52bBlOW3/zCX51EIx/tiG3LU= brendan@brendan-thinkpad";
+  pizza-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMTDDHQOMc/AKG0lF1i3C6WUVurJwQqiLk0isU57NFoN root@pizza";
   all-personal = [
     chungito
     chungito-host
     brendan-thinkpad
+    pizza-host
   ];
   # This key was originally from a Google corp laptop. I no longer have that
   # laptop but it also ended up on lots of other machines too.
