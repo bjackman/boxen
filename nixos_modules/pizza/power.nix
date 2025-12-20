fb64ea7b723870abc8e48c00fa5cb753647a09f1
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-20 15:04:43 +0100

    nixos/pizza: Break out power module

---
+++ nixos_modules/pizza/power.nix
@@ -0,0 +1,50 @@
+{
+  ...
+}:
+{
+  powerManagement.powertop.enable = true;
+
+  boot.kernelParams = [
+    # Obsessively poking around with power saving shit.
+    # Gemini got me to run `sudo nvme id-ctrl /dev/nvme0n1 -H | grep -A 15 "ps.*:"`
+    # which said "exlat:8000" for the deepest power state. The AI claims that
+    # this causes Linux to be hesitant to enter the deepest power saving state,
+    # but that setting this parameter to a higher default will fix it.
+    "nvme_core.default_ps_max_latency_us=10000"
+
+    # Gemini suggests this, but... I dunno about that one buddy.
+    # "pcie_aspm=force"
+
+    # GPU Power Management, also suggested by Gemini.
+    "i915.enable_dc=2" # Enable deeper Display Core power states
+    "i915.enable_fbc=1" # Framebuffer compression
+    "i915.enable_psr=1" # Panel Self Refresh
+  ];
+
+  # Disable some un-needed kernel modules as an attempt to try and reduce power.
+  boot.blacklistedKernelModules = [
+    "iwlwifi"
+    "btusb"
+    "uvcvideo" # Apparently USB webcames are a common culprit for blocking deep package idle.
+    # I was seeing a bunch of 'Audio codec hwC0D2: Intel' in powertop's Overview
+    # tab, disabling this driver completely makes that go away.
+    "snd_hda_intel"
+  ];
+
+  # Notes:
+  #
+  # I'm able to query DPMS (monitor power state) with:
+  #
+  #   grep . /sys/class/drm/card*-*/dpms
+  #
+  # This command will make that switch to "Off" but I don't see a dip in power
+  # usage. I suspect that while the lid is closed, there is no power usage by
+  # the monitor regardless of the logical state.
+  #
+  #   setterm --blank force --term linux < /dev/tty1 > /dev/tty1
+  #
+  # Powertop still shows that a bunch of USB devices are keeping the package
+  # from entering deep sleep. Gemini guided me to try a bunch of stuff to try
+  # and fix this but none of it works. When I rmmod xhci_pci, the power usage
+  # increases a bunch! Oh well, it's pretty low already.
+}
