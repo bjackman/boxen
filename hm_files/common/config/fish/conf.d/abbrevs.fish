dbca7bb38f82010597e724344c8132cfa8e3c97d
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-21 22:26:45 +0100

    hm/fish: Update nrs alias for new --sudo arg

--- hm_files/common/config/fish/conf.d/abbrevs.fish
+++ hm_files/common/config/fish/conf.d/abbrevs.fish
@@ -1,5 +1,5 @@
 abbr --add hms "home-manager switch"
-abbr --add nrs 'nixos-rebuild switch --flake "$HOME_MANAGER_CONFIG_CHECKOUT#$(hostname)" --use-remote-sudo'
+abbr --add nrs 'nixos-rebuild switch --flake "$HOME_MANAGER_CONFIG_CHECKOUT#$(hostname)" --sudo'
 abbr --add fnd "find . -name"
 abbr --add scu "systemctl --user"
 abbr --add ju "journalctl --user"
