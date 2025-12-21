4d95103f240a456b29a9d2b759571bd38248944b
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-21 15:09:03 +0100

    nixos/pizza: Implement BTRFS impermanence

--- nixos_modules/pizza/default.nix
+++ nixos_modules/pizza/default.nix
@@ -3,6 +3,7 @@
   pkgs,
   modulesPath,
   nixos-hardware,
+  lib,
   ...
 }:
 
@@ -53,6 +54,38 @@
   };
   services.resolved.enable = true;
 
+  # BTRFS-based impermanence.  Adapted from the example at
+  # https://github.com/nix-community/impermanence. IIUC this is mounting the
+  # root of the BTRFS subvolume hierarchy. Then it moves the /root subvolume
+  # under /old_roots. According to AI, even though this is a plain old mv
+  # operation, it actually rearranges the subvolumes. Then we create a new /root
+  # subvolume and delete old ones (older than 30d). This all happens before the
+  # rootfs gets mounted.
+  # Note the by-partlabel path is coupled with the disko configuration.n
+  boot.initrd.postResumeCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount /dev/disk/by-partlabel/disk-main-root /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
+
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
   fileSystems = {
     # Defined in disko.nix, but set neededForBoot here.
     "/persistent".neededForBoot = true;
