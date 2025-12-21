db3f218b9b5b0efbe9496086073651db1d21b9c5
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-21 21:27:11 +0100

    nixos/norte: Set PCIe DT param

--- nixos_modules/norte.nix
+++ nixos_modules/norte.nix
@@ -15,5 +15,20 @@
   # Build getting stuck at "building man-cache", try disabling that...?
   documentation.man.generateCaches = false;
 
+  hardware.raspberry-pi.config = {
+    # As per
+    # https://github.com/bjackman/nas/blob/486592769ca3fa7e186438520e745c485b116ebd/README.md?plain=1#L32
+    # (via https://docs.radxa.com/en/accessories/storage/penta-sata-hat/penta-for-rpi5#enable-pcie),
+    # need to set dtparam=pciex1 in the config.txt. There's an example of the
+    # nixos-raspberrypi Nix magic here:
+    # https://github.com/nvmd/nixos-raspberrypi/blob/develop/modules/configtxt.nix
+    # All of them seem to have "values" while this param doesn't seem to have
+    # that. Luckily I guessed this format and it did correctly update the
+    # /boot/firmware/config.txt .
+    all.base-dt-params = {
+      pciex1.enable = true;
+    };
+  };
+
   system.stateVersion = "25.11";
 }
