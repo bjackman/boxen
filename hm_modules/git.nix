14aff97eda3c86b6852388c2913754e8a7dbe55f
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-22 19:03:24 +0100

    hm/git: Fixup bad settings format
    Format changed in the 25.11 upgrade and I messed it up, need to drop the
    extraConfig bit completely.

--- hm_modules/git.nix
+++ hm_modules/git.nix
@@ -6,55 +6,53 @@
     package = pkgs.gitFull;
 
     settings = {
-      extraConfig = {
-        push = {
-          default = "upstream";
-          autoSetupRemote = true;
-        };
-
-        pull.rebase = true;
-
-        diff = {
-          renames = "true";
-          mnemonicPrefix = "true";
-          wsErrorHighlight = "all";
-          tool = "meld";
-          algorithm = "histogram";
-          colorMoved = "plain";
-        };
-
-        help.autocorrect = 3;
-
-        color.ui = "true";
-
-        rebase = {
-          autosquash = "true";
-          updateRefs = "true";
-        };
-
-        branch.sort = "-committerdate";
-        tag.sort = "version:refname";
-
-        rerere = {
-          enabled = 1;
-          autoUpdate = "true";
-        };
-
-        checkout.workers = 0; # Use num_cpu
-
-        column.ui = "auto";
-
-        fetch.prune = "true";
-
-        merge.conflictStyle = "zdiff3";
-
-        init.defaultBranch = "master";
-
-        core.commitGraph = true;
-
-        # When amending or rebasing, bring notes along, for all notes refs.
-        notes.rewriteRef = "refs/notes/*";
+      push = {
+        default = "upstream";
+        autoSetupRemote = true;
       };
+
+      pull.rebase = true;
+
+      diff = {
+        renames = "true";
+        mnemonicPrefix = "true";
+        wsErrorHighlight = "all";
+        tool = "meld";
+        algorithm = "histogram";
+        colorMoved = "plain";
+      };
+
+      help.autocorrect = 3;
+
+      color.ui = "true";
+
+      rebase = {
+        autosquash = "true";
+        updateRefs = "true";
+      };
+
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+
+      rerere = {
+        enabled = 1;
+        autoUpdate = "true";
+      };
+
+      checkout.workers = 0; # Use num_cpu
+
+      column.ui = "auto";
+
+      fetch.prune = "true";
+
+      merge.conflictStyle = "zdiff3";
+
+      init.defaultBranch = "master";
+
+      core.commitGraph = true;
+
+      # When amending or rebasing, bring notes along, for all notes refs.
+      notes.rewriteRef = "refs/notes/*";
 
       alias = {
         lgg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'";
