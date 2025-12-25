e53cb4ce51b1fe67896074d0f0a1411ccdb2a925
Brendan Jackman <bhenryj0117@gmail.com>
2025-12-25 12:50:47 +0000

    WIP: Enable watchdog (TESTME)

--- nixos_modules/server.nix
+++ nixos_modules/server.nix
@@ -7,4 +7,13 @@
 
   # There won't be a login password on this machine, all SSH all day.
   security.sudo.wheelNeedsPassword = false;
+
+  boot.kernelParams = [
+    "kernel.nmi_watchdog=1"
+    "kernel.panic=10"
+  ];
+  services.watchdogd = {
+    enable = true;
+    settings.interval = 10;
+  };
 }
