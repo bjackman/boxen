53b9df56de56ca4ce1080ceeea5f5da72fa7ea2e
Brendan Jackman <bhenryj0117@gmail.com>
2026-01-02 21:30:49 +0100

    hm/niamh: Only backup specific dirs
    Avoid backing up snaps etc

--- hm_modules/niamh.nix
+++ hm_modules/niamh.nix
@@ -13,7 +13,14 @@
         # Can't be bothered to couple this more neatly to the rest of the config
         # so we're just hard-coding the target hostname and "uploads" dir.
         repository = "sftp:${config.home.username}@norte:/uploads/restic-repo";
-        paths = [ config.home.homeDirectory ];
+        paths = map (name: "${config.home.homeDirectory}/${name}") [
+          "Desktop"
+          "Documents"
+          "Downloads"
+          "Music"
+          "Pictures"
+          "Videos"
+        ];
         passwordFile = "${pkgs.writeText "restic-repo-password.txt" "hunter2"}";
         # Note I have not actually tested if this works, I did it manually
         # with `restic -r sftp:niamh@norte:/uploads/restic-repo init`
