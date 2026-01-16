28650eedf8e9268cd97370617ef5f80b2325263a
Brendan Jackman <bhenryj0117@gmail.com>
2026-01-16 21:14:58 +0100

    nixos/chungito: Keep norte deploy-rs binary warm

--- hm_modules/chungito.nix
+++ hm_modules/chungito.nix
@@ -9,5 +9,8 @@
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
     }
+    # The deploy-rs deployment requires some Aarch64 Rust binary which means we
+    # have to compile the cross-rustc lol.
+    "github:bjackman/boxen/master#deploy.nodes.norte.profiles.system.path"
   ];
 }
