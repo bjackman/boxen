cf27918cdad9362298454b3802383cae78cedd33
Brendan Jackman <bhenryj0117@gmail.com>
2026-03-07 17:18:18 +0100

    tf/slopbox: Initial minimal example

---
+++ tf/slopbox/slopbox.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    incus = {
+      source  = "lxc/incus"
+      version = ">= 1.0.2"
+    }
+  }
+}
+
+provider "incus" {
+}
+
+# variable "nixos_image_data" {
+#   type        = string
+#   description = "Path to the built rootfs or qcow2 data file"
+# }
+
+# variable "nixos_image_metadata" {
+#   type        = string
+#   description = "Path to the built metadata tarball"
+# }
+
+# resource "incus_image" "slopbox" {
+#   source_file {
+#     data_path     = var.nixos_image_data
+#     metadata_path = var.nixos_image_metadata
+#   }
+# }
+
+resource "incus_image" "nixos" {
+  source_image = {
+    remote = "images"
+    name   = "nixos/25.11"
+    type = "virtual-machine"
+  }
+}
+
+resource "incus_instance" "slopbox" {
+  name     = "slopbox"
+  type     = "virtual-machine"
+  image    = incus_image.nixos.fingerprint
+  profiles = ["default"]
+  running  = true
+  config = {
+    "security.secureboot" = false
+  }
+}
