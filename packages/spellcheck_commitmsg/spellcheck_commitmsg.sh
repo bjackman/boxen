2d8244aabb0d98c70114b7fcd50e115147d9d00e
Brendan Jackman <jackmanb@google.com>
2026-03-20 13:34:16 +0000

    spellcheck_commitmsg.sh: Fix running with set -e

--- packages/spellcheck_commitmsg/spellcheck_commitmsg.sh
+++ packages/spellcheck_commitmsg/spellcheck_commitmsg.sh
@@ -4,7 +4,7 @@
 # GIT_SEQUENCE_EDITOR="sed -i s/^pick/reword/" GIT_EDITOR=spellcheck_commitmsg.sh git rebase -i $BASE
 
 sed '/^#/d' -i "$1"
-change_id_line=$(grep "^Change-Id" "$1")
+change_id_line=$(grep "^Change-Id" "$1" || true)
 sed '/^Change-Id/d' -i "$1"
 hunspell "$1"
 # Remove blank lines from end of file, otherwise if there's a gap between the
