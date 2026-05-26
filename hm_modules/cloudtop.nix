31740829e431db0123c64b4f9c50e50a5415adc9
Brendan Jackman <jackmanb@google.com>
2026-05-26 11:42:43 +0000

    hm/cloudtop: Remove jackmanb@google.com account
    This effectively switches the cloudtop over to using the linux.dev
    accouint same as any other machine.

--- hm_modules/cloudtop.nix
+++ hm_modules/cloudtop.nix
@@ -5,16 +5,4 @@
   ];
 
   bjackman.sashiko.enable = true;
-
-  accounts.email.accounts.corp = {
-    address = "jackmanb@google.com";
-    realName = "Brendan Jackman";
-    primary = true;
-    aerc.extraAccounts.outgoing = "/usr/bin/sendgmr -i";
-  };
-  accounts.email.accounts.linuxdev.primary = false;
-  lkml = {
-    enable = true;
-    accountRef = "corp";
-  };
 }
