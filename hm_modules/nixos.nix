714a0b6bd509b109dde76b5ae1eac05c661af854
Brendan Jackman <bhenryj0117@gmail.com>
2026-06-07 14:31:14 +0200

    hm/nixos: Encode legacy ~/.mozilla data location

--- hm_modules/nixos.nix
+++ hm_modules/nixos.nix
@@ -12,6 +12,13 @@
   bjackman.appConfigDirs.fish = [ ../hm_files/nixos/config/fish ];
   bjackman.configCheckout = "${config.home.homeDirectory}/src/boxen";
   programs.firefox.enable = true;
+  # Keeps the pre-26.05 path to avoid migrating profile data on each machine.
+  # To adopt the XDG default instead:
+  #   1. Set programs.firefox.configPath = "${config.xdg.configHome}/mozilla/firefox";
+  #   2. On each machine: mv ~/.mozilla/firefox "$XDG_CONFIG_HOME/mozilla/firefox"
+  #   3. Update the impermanence directory in nixos_modules/pc.nix from
+  #      ".mozilla/firefox" to ".config/mozilla/firefox" (or whatever $XDG_CONFIG_HOME resolves to).
+  programs.firefox.configPath = ".mozilla/firefox";
   programs.vscode.enable = true;
 
   bjackman.nix-warmups = [
