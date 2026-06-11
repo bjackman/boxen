9ff3683a31c8126754249e7198580b2a73422f67
Brendan Jackman <bhenryj0117@gmail.com>
2026-06-11 22:32:05 +0200

    hm/vscode: Fix deuplicate xdg.configFile assignment

--- hm_modules/vscode.nix
+++ hm_modules/vscode.nix
@@ -10,8 +10,6 @@
     "Code/User/settings.json" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.bjackman.configCheckout}/hm_files/common/config/Code/User/settings.json";
     };
-  };
-  xdg.configFile = {
     "Code/User/keybindings.json" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.bjackman.configCheckout}/hm_files/common/config/Code/User/keybindings.json";
     };
