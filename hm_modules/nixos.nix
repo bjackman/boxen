daaa6b34a61e3053cb5584bf605dbf1d1b7bc269
Brendan Jackman <bhenryj0117@gmail.com>
2026-08-06 22:20:23 +0200

    hm: Move agentHostContext notes about NixOS centrally

--- hm_modules/nixos.nix
+++ hm_modules/nixos.nix
@@ -8,7 +8,9 @@
   imports = [
     ./dark-mode.nix
     ./nix-warmup.nix
+    ./agent-host-context.nix
   ];
+
   bjackman.appConfigDirs.fish = [ ../hm_files/nixos/config/fish ];
   bjackman.configCheckout = "${config.home.homeDirectory}/src/boxen";
   programs.firefox.enable = true;
@@ -24,4 +26,14 @@
   bjackman.nix-warmups = [
     "github:bjackman/boxen/master#nixosConfigurations.${osConfig.networking.hostName}.config.system.build.toplevel"
   ];
+
+  bjackman.agentHostContext = ''
+    You're running on a NixOS host. I tend not to install much into the global
+    environment, e.g. you might find that there's no `python3` in $PATH. But,
+    you are free to run stuff from nixpkgs. This is a fully flake-based system
+    so you can do that with `nix run nixpkgs#<package>`. If you need the latest
+    version, use `nix run nixpkgs-unstable#<package>` instead. You can also use
+    `nix shell nixpkgs#<package> -c <command>` to run commands in a shell that
+    has the package installed.
+  '';
 }
--- hm_modules/pc.nix
+++ hm_modules/pc.nix
@@ -14,13 +14,5 @@
     (${osConfig.networking.hostName}), configured from the "boxen" flake checked
     out at `${config.bjackman.configCheckout}`, if you need to understand how
     the system is set up you can read that config.
-
-    On NixOS I tend not to install much into the global environment, e.g. you
-    might find that there's no `python3` in $PATH. But, you are free to run
-    stuff from nixpkgs. This is a fully flake-based system so you can do that
-    with `nix run nixpkgs#<package>`. If you need the latest version, use `nix
-    run nixpkgs-unstable#<package>` instead. You can also use `nix shell
-    nixpkgs#<package> -c <command>` to run commands in a shell that has the
-    package installed.
   '';
 }
