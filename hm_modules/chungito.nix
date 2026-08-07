dbc781c0f4572e6f838f4e2f54a7a642fddb8882
Brendan Jackman <bhenryj0117@gmail.com>
2026-08-07 18:12:25 +0200

    hm/chungito: Set up subtituter for norte system path too
    For some reason this includes a kernel too so allow that to be fetched
    from cachix.

--- hm_modules/chungito.nix
+++ hm_modules/chungito.nix
@@ -10,20 +10,27 @@
     ./pc.nix
   ];
 
-  bjackman.nix-warmups = [
-    {
-      flakeRef = "github:bjackman/boxen/master#nixosConfigurations.norte.config.system.build.toplevel";
+  bjackman.nix-warmups =
+    let
       buildArgs = [
         "--extra-substituters"
         "https://nixos-raspberrypi.cachix.org"
         "--extra-trusted-public-keys"
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
-    }
-    # The deploy-rs deployment requires some Aarch64 Rust binary which means we
-    # have to compile the cross-rustc lol.
-    "github:bjackman/boxen/master#deploy.nodes.norte.profiles.system.path"
-  ];
+    in
+    [
+      {
+        flakeRef = "github:bjackman/boxen/master#nixosConfigurations.norte.config.system.build.toplevel";
+        inherit buildArgs;
+      }
+      {
+        # The deploy-rs deployment requires some Aarch64 Rust binary which means we
+        # have to compile the cross-rustc lol.
+        flakeRef = "github:bjackman/boxen/master#deploy.nodes.norte.profiles.system.path";
+        inherit buildArgs;
+      }
+    ];
 
   # https://wiki.nixos.org/wiki/Zed#Remote_server
   home.file.".zed_server" = {
